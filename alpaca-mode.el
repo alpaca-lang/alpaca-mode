69dfb34ee731fa2e29433810098110e4a8910933
Tyler Weir <tyler.weir@gmail.com>
2016-12-07 15:59:42 -0500

    Merge @tjweir's test.mfle as test.alp
    Source: https://github.com/tjweir/alpaca-mode/blob/549d240a3696e69fe2fce876a7f52c9161afbe62/test.mfle

--- alpaca-mode.el
+++ alpaca-mode.el
@@ -14,7 +14,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-;; Author: Eric Bailey
+;; Author: Eric Bailey, Tyler Weir
 ;; Keywords: languages alpaca
 
 ;;; Commentary:
